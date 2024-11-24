c1f12d2271155c72fc4624f5508446b1003bc299
ACLN RARE <105054059+ACLNYT@users.noreply.github.com>
2024-11-25 00:35:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Array of names to pick from
-const names = ["Alice", "rubik", " tevx", "aleksey","Bob", "Charlie", "Diana", "Eve", "Frank", "Grace"];
+const names = ["Alice", "rubik", " tevx", "aleksey","Bob", "Charlie", "Diana", "Eve", "Frank", "Grace"," crimson ","anik"
 
 // Function to pick a random name
 function pickRandomName() {
